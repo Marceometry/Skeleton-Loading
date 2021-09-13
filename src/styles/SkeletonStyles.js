7c6bb2d074d78b7a02814cb4cd5078ff66618f0a
Marceometry <marceometry@gmail.com>
2021-09-13 08:44:17 -0300

    add border animation

--- src/styles/SkeletonStyles.js
+++ src/styles/SkeletonStyles.js
@@ -5,9 +5,18 @@ const SkeletonStyles = createGlobalStyle`
     0% { background: var(--skeleton-color-1) }
     100% { background: var(--skeleton-color-2) }
   }
+  
+  @keyframes skeleton-border-animation {
+    0% { border-color: var(--skeleton-color-1) }
+    100% { border-color: var(--skeleton-color-2) }
+  }
 
   .skeleton {
     animation: skeleton-animation 1s linear infinite alternate;
+  }
+
+  .skeleton-border {
+    animation: skeleton-border-animation 1s linear infinite alternate;
   }
 `
 
