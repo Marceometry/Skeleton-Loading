706088ce3de6fa79a35e6a51c8c3a2b13b9ac897
Marceometry <marceometry@gmail.com>
2021-09-13 08:44:53 -0300

    change title

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,22 +1,22 @@
 import { Button } from '../Button'
-import { SkeletonCard } from '../Card/skeleton'
+import { Card } from '../Card'
 import StyledMain from './styles'
 
 export function Main() {
   return (
     <StyledMain>
       <div className='main-header'>
-        <h1>Lorem</h1>
+        <h2>Skeleton Loading Example in Cards</h2>
 
         {/* <Button>Refresh</Button> */}
       </div>
       
       <div className='grid'>
-        <SkeletonCard />
-        <SkeletonCard />
-        <SkeletonCard />
-        <SkeletonCard />
-        <SkeletonCard />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </StyledMain>
   )
